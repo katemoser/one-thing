7e535b848e4ebfc30010d43c7ccdc05fa4f83163
Kate Moser <komoser@gmail.com>
2024-07-10 14:05:56 -0700

    added endpoint to get user's next assignment

--- app/api/users/[username]/assignments/next/route.ts
+++ app/api/users/[username]/assignments/next/route.ts
@@ -1,1 +1,10 @@
 /** GET next assignment for this user */
+
+import { selectNextAssignment } from "@/app/actions";
+
+export async function GET(request: Request, { params }: { params: { username: string; }; }){
+
+  const assignment = await selectNextAssignment(params.username)
+  console.log("assignment:", assignment);
+  return Response.json({assignment})
+}
